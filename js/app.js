a4ad3ca43fc17f038652824ae04c95514131c8bc
tainanklc <61124590+tainanklc@users.noreply.github.com>
2021-12-06 11:49:44 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,48 @@
+import { valida } from './validacao.js'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Unix timestamp (in seconds) to count down to
+    var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 7) + 1;
+  
+    // Set up FlipDown
+    var flipdown = new FlipDown(twoDaysFromNow, {
+        headings: ["Dias", "Horas", "Minutos", "Segundos"],
+      })
+  
+      // Start the countdown
+      .start()
+  
+      // Do something when the countdown ends
+      .ifEnded(() => {
+        console.log('The countdown has ended!');
+      });
+});
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(input => {
+    if(input.dataset.tipo === 'preco') {
+        SimpleMaskMoney.setMask(input, {
+            prefix: 'R$ ',
+            fixed: true,
+            fractionDigits: 2,
+            decimalSeparator: ',',
+            thousandsSeparator: '.',
+            cursor: 'end'
+        })
+    }
+
+    input.addEventListener('blur', (evento) => {
+        valida(evento.target)
+    })
+})
+
+function criarPopup(){
+    newWindow = window.open ('', 'pagina');
+    newWindow.document.write ("Este é um novo popup <br/> <img src='imagem.jpg' width='100' height='100'>");
+}
+
+const novaTarefa = document.querySelector('[data-botao]')
+novaTarefa.addEventListener('click',criarPopup())
---
+++ js/validacao.js
@@ -0,0 +1,42 @@
+export function valida(input) {
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
+}
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo de nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    }
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    
+    return mensagem
+}
+
+
+
+
+  
