259609d6b82ab774a344f408c7d38fdb4749e7da
Tainan Calixto <tainankhalil@gmail.com>
2021-12-08 00:50:18 -0300

    Atualização Arthur

--- js/app.js
+++ js/app.js
@@ -40,8 +40,6 @@ inputs.forEach(input => {
     })
 })
 
-const novaTarefa = document.getElementById("enviarEmail")
-
 // novaTarefa.addEventListener('click', montaEmail)
 // [montaEmail, abreJanela('http://www.xtf.dk', 'xtf', 900, 500)])
 
@@ -57,3 +55,41 @@ const novaTarefa = document.getElementById("enviarEmail")
 
 // const novaTarefa = document.querySelector('[data-botao]')
 // novaTarefa.addEventListener('click',criarPopup())
+
+(function () {
+    if (document.getElementsByTagName('form').length > 0) {
+        document.getElementsByTagName('form')[0].addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            // Check for spam
+            if(document.getElementById('js-validate-robot').value !== '') { return false }
+
+            // Get url for mailchimp
+            var url = this.action.replace('/post?', '/post-json?');
+
+            // Add form data to object
+            var data = '';
+            var inputs = this.querySelectorAll('#js-form-inputs input');
+            for (var i = 0; i < inputs.length; i++) {
+                data += '&' + inputs[i].name + '=' + encodeURIComponent(inputs[i].value);
+            }
+
+            // Create & add post script to the DOM
+            var script = document.createElement('script');
+            script.src = url + data;
+            document.body.appendChild(script);
+
+            // Callback function
+            var callback = 'callback';
+            window[callback] = function(data) {
+
+                // Remove post script from the DOM
+                delete window[callback];
+                document.body.removeChild(script);
+
+                // Display response message
+                document.getElementById('js-subscribe-response').innerHTML = data.msg
+            };
+        });
+    }
+})();
